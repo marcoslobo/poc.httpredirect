4350ba199c13d4c04b2822e00426cb72cfe4b4a2
Marcos Lobo <marcos.lobo.nit@gmail.com>
2020-06-08 21:12:07 -0300

    teste

---
+++ POC.HttpRedirect/Controllers/WeatherForecastController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace POC.HttpRedirect.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public void Get()
+        {
+            HttpContext.Response.Cookies.Append("jSessionId", "ronaldo");
+            HttpContext.Response.Redirect("https://www.google.com.br");            
+        }
+    }
+}
